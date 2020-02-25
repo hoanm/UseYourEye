018ebf1a160479609db90c9b0a7017d4c6a56bfb
hoanm <mr.nmh175@gmail.com>
2020-02-25 14:35:03 +0700

    + Add Help
    Add function for Help button.

--- Assets/Scripts/HelpClick.cs
+++ Assets/Scripts/HelpClick.cs
@@ -3,13 +3,23 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
+using System;
+using System.Globalization;
+
 public class HelpClick : MonoBehaviour
 {
     public Button btn;
+	public AudioSource audio;
 
     // Update is called once per frame
     public void PlayAlarm(){
 		Debug.Log ("You have clicked the Help button!");
 		// Play an alarm signal
+		audio = GetComponent<AudioSource>();
+		audio.Play();
+		// Need FIX: Play sound in 30s !
+		//System.Threading.Thread.Sleep(30000);
+		//audio.Pause();
 	}
+
 }
